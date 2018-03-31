89efdad68c1fb52819963be4f59be09a8549bee2
chokey11 <chokey1992@gmail.com>
2018-04-01 00:11:16 +0300

    Add files via upload

--- JSLesson2/JS/script.js
+++ JSLesson2/JS/script.js
@@ -1,6 +1,6 @@
 var num = "33721";
-Numb = (num[0] * num[1] * num[2] * num[3] * num[4]);
+Number = (num[0] * num[1] * num[2] * num[3] * num[4]);
 
-console.log(Numb**3);
+console.log(Number);
 
-alert(Numb**3);
+alert(Number**3);
